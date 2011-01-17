719a3e90f2cc8a764f057007ef7fac8c11f184d7
Daniel Berger <djberg96@gmail.com>
2011-01-17 08:46:38 -0700

    Skip two tests on Windows XP.

--- test/test_win32_sapi5.rb
+++ test/test_win32_sapi5.rb
@@ -10,94 +10,96 @@
 #
 # You should run this test case via the 'rake test' Rake task.
 #############################################################################
+require 'rubygems'
+gem 'test-unit'
+
 require 'win32/sapi5'
+require 'windows/system_info'
 require 'test/unit'
 include Win32
 
 class TC_Win32_SAPI5 < Test::Unit::TestCase
-   def setup
-   end
+  include Windows::SystemInfo
+
+  def test_version
+    assert_equal('0.1.6', SAPI5::VERSION)
+  end
    
-   def test_version
-      assert_equal('0.1.5', SAPI5::VERSION)
-   end
+  def test_SpAudioFormat
+    assert_nothing_raised{ SpAudioFormat.new }
+  end
+  
+  def test_SpCustomStream
+    assert_nothing_raised{ SpCustomStream.new }
+  end
+  
+  def test_SpFileStream
+    assert_nothing_raised{ SpFileStream.new }
+  end
    
-   def test_SpAudioFormat
-      assert_nothing_raised{ SpAudioFormat.new }
-   end
+  def test_SpInProcRecoContext
+    omit_if(windows_xp?, "SpInProcRecoContext skipped on Windows XP")
+    assert_nothing_raised{ SpInProcRecoContext.new }
+  end
    
-   def test_SpCustomStream
-      assert_nothing_raised{ SpCustomStream.new }
-   end
+  def test_SpInProcRecognizer
+    assert_nothing_raised{ SpInProcRecognizer.new }
+  end
    
-   def test_SpFileStream
-      assert_nothing_raised{ SpFileStream.new }
-   end
+  def test_SpLexicon
+    assert_nothing_raised{ SpLexicon.new }
+  end
    
-   def test_SpInProcRecoContext
-      assert_nothing_raised{ SpInProcRecoContext.new }
-   end
+  def test_SpMemoryStream
+    assert_nothing_raised{ SpMemoryStream.new }
+  end
    
-   def test_SpInProcRecognizer
-      assert_nothing_raised{ SpInProcRecognizer.new }
-   end
+  def test_SpMMAudioIn
+    assert_nothing_raised{ SpMMAudioIn.new }
+  end
    
-   def test_SpLexicon
-      assert_nothing_raised{ SpLexicon.new }
-   end
+  def test_SpMMAudioOut
+    assert_nothing_raised{ SpMMAudioOut.new }
+  end
    
-   def test_SpMemoryStream
-      assert_nothing_raised{ SpMemoryStream.new }
-   end
+  def test_SpObjectToken
+    assert_nothing_raised{ SpObjectToken.new }
+  end
    
-   def test_SpMMAudioIn
-      assert_nothing_raised{ SpMMAudioIn.new }
-   end
+  def test_SpObjectTokenCategory
+    assert_nothing_raised{ SpObjectTokenCategory.new }
+  end
    
-   def test_SpMMAudioOut
-      assert_nothing_raised{ SpMMAudioOut.new }
-   end
+  def test_SpPhoneConverter
+    assert_nothing_raised{ SpPhoneConverter.new }
+  end
    
-   def test_SpObjectToken
-      assert_nothing_raised{ SpObjectToken.new }
-   end
+  def test_SpPhraseInfoBuilder
+    assert_nothing_raised{ SpPhraseInfoBuilder.new }
+  end
    
-   def test_SpObjectTokenCategory
-      assert_nothing_raised{ SpObjectTokenCategory.new }
-   end
+  def test_SpSharedRecoContext
+    omit_if(windows_xp?, "SpSharedRecoContext skipped on Windows XP")
+    assert_nothing_raised{ SpSharedRecoContext.new }
+  end
    
-   def test_SpPhoneConverter
-      assert_nothing_raised{ SpPhoneConverter.new }
-   end
+  def test_SpSharedRecognizer
+    assert_nothing_raised{ SpSharedRecognizer.new }
+  end
    
-   def test_SpPhraseInfoBuilder
-      assert_nothing_raised{ SpPhraseInfoBuilder.new }
-   end
+  def test_SpTextSelectionInformation
+    assert_nothing_raised{ SpTextSelectionInformation.new }
+  end
    
-   def test_SpSharedRecoContext
-      assert_nothing_raised{ SpSharedRecoContext.new }
-   end
+  def test_SpUnCompressedLexicon
+    assert_nothing_raised{ SpUnCompressedLexicon.new }
+  end
    
-   def test_SpSharedRecognizer
-      assert_nothing_raised{ SpSharedRecognizer.new }
-   end
+  def test_SpVoice
+    assert_nothing_raised{ SpVoice.new }
+  end
    
-   def test_SpTextSelectionInformation
-      assert_nothing_raised{ SpTextSelectionInformation.new }
-   end
-   
-   def test_SpUnCompressedLexicon
-      assert_nothing_raised{ SpUnCompressedLexicon.new }
-   end
-   
-   def test_SpVoice
-      assert_nothing_raised{ SpVoice.new }
-   end
-   
-   def test_SpWaveFormatEx
-      assert_nothing_raised{ SpWaveFormatEx.new }
-   end
-   
-   def teardown
-   end
+  def test_SpWaveFormatEx
+    assert_nothing_raised{ SpWaveFormatEx.new }
+  end
 end
